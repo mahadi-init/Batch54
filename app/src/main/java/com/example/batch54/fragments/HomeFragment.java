23758cd6edfe1da26c3fbaec073697b9b7b54597
Mahadi Hasan <me.mahadi@outlook.com>
2022-06-02 17:32:21 +0600

    added main layout fragment, menu options

---
+++ app/src/main/java/com/example/batch54/fragments/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.batch54.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.batch54.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
