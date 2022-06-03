c0ba964cc6a8fa721f58a8604bd4f0a3a686d7b2
Mahadi Hasan <me.mahadi@outlook.com>
2022-06-03 15:17:56 +0600

    topic name changed to trending

--- app/src/main/java/com/example/batch54/fragments/TrendingFragment.java
+++ app/src/main/java/com/example/batch54/fragments/TrendingFragment.java
@@ -2,7 +2,6 @@ package com.example.batch54.fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,12 +10,12 @@ import android.view.ViewGroup;
 
 import com.example.batch54.R;
 
-public class TopicFragment extends Fragment {
+public class TrendingFragment extends Fragment {
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_topic, container, false);
+        return inflater.inflate(R.layout.fragment_trending, container, false);
     }
 }
