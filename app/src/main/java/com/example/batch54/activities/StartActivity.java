ab45b06952085ecd62aaa25c4a0be9f739f49d06
Mahadi Hasan <me.mahadi@outlook.com>
2022-06-09 02:03:11 +0600

    designed the chat layout

--- app/src/main/java/com/example/batch54/activities/StartActivity.java
+++ app/src/main/java/com/example/batch54/activities/StartActivity.java
@@ -10,12 +10,16 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.example.batch54.databinding.ActivityStartBinding;
+import com.example.batch54.viewmodels.HomeFragmentViewmodel;
+import com.example.batch54.viewmodels.StoryLayoutViewmodel;
 
 public class StartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,11 @@ public class StartActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.WHITE);
         ActivityStartBinding binding = ActivityStartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // cache the data on runtime to save load time
+        HomeFragmentViewmodel viewmodel = new ViewModelProvider(this).get(HomeFragmentViewmodel.class);
+        Thread thread = new Thread(viewmodel);
+        thread.start();
 
         //set animation to a targeted view
         YoYo.with(Techniques.FadeIn)
